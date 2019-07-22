dd633bc7fb193b5a951d041ec27a9153cfed3a82
Madeline Young <madelinejy@gmail.com>
2019-07-22 17:44:47 +0000

    Created Course Validations for Course Info Entry
    Former-commit-id: 0e4dbdc3ea3314d41b8a14014fc32515786a01b3

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,9 +6,13 @@ class Instructor::CoursesController < ApplicationController
 	end
 
 	def create
-		@course = current_user.courses.create(course_params)
-    	redirect_to instructor_course_path(@course)
-	end
+    @course = current_user.courses.create(course_params)
+	    if @course.valid?
+	      redirect_to instructor_course_path(@course)
+	    else
+	      render :new, status: :unprocessable_entity
+	    end
+  	end
 
 	def show
 		@course = Course.find(params[:id])
@@ -18,4 +22,5 @@ class Instructor::CoursesController < ApplicationController
 
 	def course_params
 		params.require(:course).permit(:title, :description, :cost)
+	end
 end
